62063ced633e016b4bc3824501b39db991310b6d
zul-adn <zbojetx@gmail.com>
2022-12-07 13:57:16 +0700

    Fixing carousel

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -19,12 +19,9 @@ export default function Index(props) {
 
   return (
     <div className={`w-full top-0 absolute h-3/5 bg-slate-200 -z-10`}>
-      <Carousel autoPlay className={`w-full h-full`}>
-        {images?.data
-          .filter((image) => image.status)
-          .map((imag, i) => (
+   ?
             <img
-              key={i}
+            
               // src={`${backendUrl}/banner/${imag.filename}`}
               src={
                 "https://images.unsplash.com/photo-1620561099217-990c648c8502?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
@@ -32,10 +29,9 @@ export default function Index(props) {
               // height={300}
               // width={300}
               className={`w-full h-full object-cover`}
-              alt={imag.alt}
+              alt={'alt'}
             />
-          ))}
-      </Carousel>
+       ?
     </div>
   );
 }
