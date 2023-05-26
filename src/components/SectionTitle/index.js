9f22a22414c715e995af342b9b09d04ba387ee95
zul-adn <zbojetx@gmail.com>
2023-05-27 00:39:42 +0700

    fix color

--- src/components/SectionTitle/index.js
+++ src/components/SectionTitle/index.js
@@ -4,7 +4,7 @@ export default function Index(props) {
   const { title, subtitle } = props;
   return (
     <div className={`max-w-5xl mx-auto   mt-20 bg-white `}>
-      <h1 className={"text-4xl font-bold"}>{title}</h1>
+      <h1 className={"text-4xl font-bold text-black"}>{title}</h1>
       <p className={"mt-2 text-gray-600 text-xl"}>{subtitle}</p>
     </div>
   );
